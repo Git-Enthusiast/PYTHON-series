59139e966a7e79d344e190809fcff7f76e964123
Git-Enthsiast <messachuate@gmail.com>
2023-09-05 13:00:52 +0530

    bitwise operator

---
+++ Python_series/bitwise_operator.py
@@ -0,0 +1,15 @@
+a= 10
+b=8
+print( "The binary of 10 is:",bin(a))
+print("The binary of 8 is:",bin(b))
+print("The binary of a&b is:",bin(a&b),"and The decimal value of a&b is:",a&b)
+print("The binary of a|b is:",bin(a|b),"and The decimal value of a&b is:",a|b)
+print("The binary of a^b is:",bin(a^b),"and The decimal value of a&b is:",a^b)
+
+
+
+# 10 in binary: 0b1010
+#  8 in binary: 0b1000
+#  & in binary:   1000 in decimal: 8
+#  | in binary:   1010 in decimal: 10
+#  ^ in binary:   0010 in decimal: 2
