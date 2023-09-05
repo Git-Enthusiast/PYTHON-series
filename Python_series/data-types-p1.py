81afcafa10359e84de90eea3df2991ee93408f71
Git-Enthsiast <messachuate@gmail.com>
2023-09-06 03:53:17 +0530

    Data types p1 update

--- Python_series/data-types-p1.py
+++ Python_series/data-types-p1.py
@@ -31,3 +31,15 @@ a='''
 
   '''
 print(a,"is a type",type(a))
+a ='10'
+print(a,type(a))
+
+#list -> is an ordered sequence of items and is flexible means able to change data inside it.
+l = [2,2.2,'Rajan']
+print(l,type(l))
+l[1]=9.2
+print(l,type(l))
+
+# tuple
+# no difference between list and tuple except  you can not change value 
+# in tuple it is immutable and initilized as (,,) it is also ordered sequence of items.
