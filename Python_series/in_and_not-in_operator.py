40971be8ed7fec79173e081a2872ddbfb348a7ef
Git-Enthsiast <messachuate@gmail.com>
2023-09-05 12:28:41 +0530

    operator in python

---
+++ Python_series/in_and_not-in_operator.py
@@ -0,0 +1,13 @@
+string1 ="Hello"
+print('h' in string1)
+print('H' in string1) #python is a case sensative language.
+print("o" not in string1)
+print("O" not in string1)
+
+
+
+print('List usuage')
+list1 = [20,40,29,92,93,91,84]
+print(50 in list1)
+print(92 in list1)
+print(87 not in list1)
